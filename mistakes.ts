5f93afc5b19ca88bebd99650ff9756f8d9004957
Deepak Bharti <deepak.bharti@collegedunia.com>
2020-09-27 20:12:59 +0530

    rename

---
+++ mistakes.ts
@@ -0,0 +1,4 @@
+// not converting values to string while getting value from refs, caused memory crash
+
+// while drawing the hexagon to canvas, I was adding hexagons out of the canvas and they were not visible and I could know it after too long
+// didn't focus there
