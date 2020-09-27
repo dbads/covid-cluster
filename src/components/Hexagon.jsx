782ec5527a757127df17c19efa4688fe0de68c25
Deepak Bharti <deepak.bharti@collegedunia.com>
2020-09-27 23:13:17 +0530

    added the initial addition of hexagon

--- src/components/Hexagon.jsx
+++ src/components/Hexagon.jsx
@@ -9,17 +9,34 @@ class Hexagon extends Component {
     this.canvasRef = React.createRef();
     this.hexagonToAdd = React.createRef();
     this.hexagonToRemove = React.createRef();
-    this.xCordinate = React.createRef();
-    this.yCordinate = React.createRef();
+    this.sideNoRef = React.createRef();
+    this.colours = { 0: "red", 1: "green", 2: "brown", 3: "blue", 4: "purple", 5: "pink" };
+    this.sides = new Map();
     this.state = {
-      hexSize: 20,
+      hexSize: 15,
       hexagons: {},
+      // hexagons: { a: { center: { x: 50, y: 50 } }, b: { center: { x: 10, y: 10 } } },
     };
+    /* hexagons format
+    {
+      [hexName] : {
+        center: { x: 50, y: 50}, // center coordinate
+        sides: {
+          0: {
+            start: { x: 100, y: 100},
+            end: { x: 150, y: 150}
+          },
+          // ..... all sides similarly
+        }
+      }
+    }
+    */
   }
 
   componentDidMount() {
     console.log("Current Hexagons", this.state.hexagons);
-    this.drawHex({ x: 200, y: 100 });
+    this.drawHex();
+    // this.drawCluster();
   }
 
   componentDidUpdate() {
@@ -27,16 +44,21 @@ class Hexagon extends Component {
     this.drawCluster();
   }
 
+  getCenter = (start, end, existingHexCenter = undefined) => {
+    return { x: (start.x + end.x) / 2, y: (start.y + end.y) / 2 };
+  };
+
   getActionInputs = () => {
-    // const startX = Number(this.xCordinate.current.input.value);
-    // const startY = Number(this.yCordinate.current.input.value);
+    const sideNo = this.sideNoRef.current.input.value;
     const hexagonToAdd = this.hexagonToAdd.current.input.value;
     const hexagonToRemove = this.hexagonToRemove.current.input.value;
-    // let center;
-    // if (startX && startY) {
-    const center = { x: 200, y: 100 };
-    // }
-    return { center, hexagonToAdd, hexagonToRemove, canvas: this.canvasRef.current };
+    let center = { x: 100, y: 100 };
+    if (sideNo) {
+      const { start, end } = this.state.hexagons[hexagonToAdd].sides[sideNo];
+      center = this.getCenter(start, end);
+    }
+
+    return { sideNo, center, hexagonToAdd, hexagonToRemove, canvas: this.canvasRef.current };
   };
 
   /**
@@ -45,24 +67,38 @@ class Hexagon extends Component {
    */
   getHexCornerCord = (center, corner) => {
     const { hexSize } = this.state;
-    const angleDegree = 60 * corner + 30;
+    const angleDegree = 60 * corner + 60;
     const angleRadian = (Math.PI / 180) * angleDegree;
     const x = center.x + hexSize * Math.cos(angleRadian);
     const y = center.y + hexSize * Math.sin(angleRadian);
     return { x, y };
   };
 
-  drawHex = (center) => {
-    console.log("Drawing Hexagon at center", center);
+  getHexSide = (center) => {
+    const sides = {};
     for (let i = 0; i < 6; i += 1) {
       const startCord = this.getHexCornerCord(center, i);
       const endCord = this.getHexCornerCord(center, i + 1);
-
-      this.drawLine(startCord, endCord);
+      sides[i] = { startCord, endCord };
     }
+    console.log(sides, "sides");
+    return sides;
   };
 
-  drawLine = (startCord, endCord) => {
+  drawHex = (hexName, center = { x: 100, y: 100 }) => {
+    // console.log("Drawing Hexagon at center", center);
+    let sides;
+    if (hexName) sides = this.state.hexagons[hexName].sides;
+    else {
+      sides = this.getHexSide(center);
+    }
+    Object.entries(sides).map((side) => {
+      console.log(side, "side-");
+      return this.drawLine(side[1].startCord, side[1].endCord, Number(side[0]));
+    });
+  };
+
+  drawLine = (startCord, endCord, sideNo) => {
     console.log(
       "Drawing Line from ",
       startCord,
@@ -71,27 +107,44 @@ class Hexagon extends Component {
       "canvas -",
       this.canvasRef.current
     );
+    const lineMid = { x: (startCord.x + endCord.x) / 2, y: (startCord.y + endCord.y) / 2 };
+    // const color = this.colours[sideNo];
     const context = this.canvasRef.current.getContext("2d");
+    // context.translate(0.5, 0.5);
     context.beginPath();
-    context.color = "red";
+    // context.strokeStyle = color;
     context.moveTo(startCord.x, startCord.y);
     context.lineTo(endCord.x, endCord.y);
+    context.font = "5px Verdana";
+    context.strokeText(sideNo, lineMid.x + 1, lineMid.y + 1);
+    // context.translate(-0.5, -0.5);
     context.stroke();
     context.closePath();
   };
 
-  addHex = () => {
-    const { center, hexagonToAdd } = this.getActionInputs();
-    console.log("Hexagon to Add - ", center, hexagonToAdd);
+  addHex = (center, sides) => {
+    const { hexagonToAdd, sideNo } = this.getActionInputs();
+    console.log("Hexagon to Add - ", hexagonToAdd, sideNo);
 
-    if (center && hexagonToAdd) {
+    if (hexagonToAdd && sideNo) {
       if (!this.state.hexagons[hexagonToAdd]) {
-        const newHexagon = { [hexagonToAdd]: { center } };
+        const existingHex = this.state.hexagons[hexagonToAdd];
+        const { start, end } = existingHex.sides[sideNo];
+        const existingHexCenter = existingHex.center;
+        center = this.getCenter(start, end, existingHexCenter);
+
+        const newHexagonSides = this.getHexSide(center);
+        const newHexagon = { [hexagonToAdd]: { center }, ...newHexagonSides };
         this.setState((state) => ({ hexagons: { ...state.hexagons, ...newHexagon } }));
       }
       else {
         console.log("Hexagon already added");
       }
+    }
+    else if (center && sides) {
+      // when adding first cluster, added during mounting, initial hexagon
+      const newHexagon = { a: { center }, ...sides };
+      this.setState((state) => ({ hexagons: { ...state.hexagons, ...newHexagon } }));
     }
     else {
       console.log("Details are required");
@@ -100,10 +153,6 @@ class Hexagon extends Component {
 
   drawCluster = () => {
     Object.entries(this.state.hexagons).map((hexagon) => this.drawHex(hexagon[1].center));
-  };
-
-  onChange = () => {
-    console.log("canvas changed");
   };
 
   render() {
@@ -117,8 +166,7 @@ class Hexagon extends Component {
         <Form layout="vertical">
           <Row key="addHexagonInputs">
             <Input placeholder="Name of hexagon" ref={this.hexagonToAdd} />
-            <Input name="statX" placeholder="X coordinate" ref={this.xCordinate} />
-            <Input name="startY" placeholder="Y coordinate" ref={this.yCordinate} />
+            <Input name="statX" placeholder="Border No" ref={this.sideNoRef} />
             <Button onClick={this.addHex}>Add Hexagon</Button>
           </Row>
           <Row key="removeHexagonInputs">
